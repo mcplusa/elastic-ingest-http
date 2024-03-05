9801f87af6efc595c71b981599ab0cf954929c42
steph van schalkwyk <md4668w4c5qrknts4z5r6xut2zheo3@bots.bitbucket.org>
2024-03-05 11:38:26 -0600

    Added CustomSettings

--- src/main/java/org/elasticsearch/plugin/ingest/rest/IngestRestPlugin.java
+++ src/main/java/org/elasticsearch/plugin/ingest/rest/IngestRestPlugin.java
@@ -10,40 +10,31 @@ import org.elasticsearch.plugins.IngestPlugin;
 import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.ingest.Processor;
 import org.elasticsearch.common.settings.Setting;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.env.Environment;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class IngestRestPlugin extends Plugin implements IngestPlugin {
-/*    public String name() {
-        return "ingest-rest";
-    }
 
-    public String description() {
-        return "Ingest Rest Plugin";
+    private final ExampleCustomSettingsConfig config;
+
+    public IngestRestPlugin(Settings settings, Path configPath) {
+        this.config = new ExampleCustomSettingsConfig(new Environment(settings, configPath));
     }
-*/    
-   public static final Setting<String> YOUR_SETTING =
-            new Setting<>("ingest.awesome.setting", "foo", (value) -> value, Setting.Property.NodeScope);
 
     @Override
     public List<Setting<?>> getSettings() {
-        return Arrays.asList(YOUR_SETTING);
+        return Arrays.asList(ExampleCustomSettingsConfig.SIMPLE_SETTING,
+                             ExampleCustomSettingsConfig.BOOLEAN_SETTING,
+                             ExampleCustomSettingsConfig.VALIDATED_SETTING,
+                             ExampleCustomSettingsConfig.FILTERED_SETTING,
+                             ExampleCustomSettingsConfig.SECURED_SETTING,
+                             ExampleCustomSettingsConfig.LIST_SETTING);
     }    
 
-        /**
-     * Returns additional ingest processor types added by this plugin.
-     *
-     * The key of the returned {@link Map} is the unique name for the processor which is specified
-     * in pipeline configurations, and the value is a {@link org.elasticsearch.ingest.Processor.Factory}
-     * to create the processor from a given pipeline configuration.
-     */
-
-    /*@Override
-    public Map<String, Processor.Factory> getProcessors(Processor.Parameters parameters) {
-        return Map.<String, Processor.Factory>of(
-            IngestRestProcessor.TYPE, new IngestRestProcessor.Factory()
-        );
-    }
-*/
     @Override
     public Map<String, Processor.Factory> getProcessors(Processor.Parameters parameters) {
       Map<String, Processor.Factory> processors = new HashMap<>();
