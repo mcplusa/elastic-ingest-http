25043bd97590b5985d9c5efb3cfc4d802e06e280
steph van schalkwyk <md4668w4c5qrknts4z5r6xut2zheo3@bots.bitbucket.org>
2024-03-06 15:35:28 -0600

    Added more RST call functionality. Getting 500

---
+++ src/main/java/org/elasticsearch/plugin/ingest/rest/HttpClient.java
@@ -0,0 +1,155 @@
+package org.elasticsearch.plugin.ingest.rest;
+
+import org.elasticsearch.ElasticsearchStatusException;
+import org.elasticsearch.ResourceNotFoundException;
+import org.elasticsearch.SpecialPermission;
+import org.elasticsearch.common.CheckedSupplier;
+import org.elasticsearch.core.SuppressForbidden;
+import org.elasticsearch.rest.RestStatus;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.security.AccessController;
+import java.security.PrivilegedActionException;
+import java.security.PrivilegedExceptionAction;
+import java.util.Map;
+
+import static java.net.HttpURLConnection.HTTP_MOVED_PERM;
+import static java.net.HttpURLConnection.HTTP_MOVED_TEMP;
+import static java.net.HttpURLConnection.HTTP_NOT_FOUND;
+import static java.net.HttpURLConnection.HTTP_OK;
+import static java.net.HttpURLConnection.HTTP_SEE_OTHER;
+
+
+@SuppressWarnings("removal")
+class HttpClient {
+
+    byte[] getBytes(String url) throws IOException {
+        return get(url).readAllBytes();
+    }
+
+    InputStream get(String urlToGet) throws IOException {
+        return doPrivileged(() -> {
+            String url = urlToGet;
+            HttpURLConnection conn = createConnection(url);
+            int redirectsCount = 0;
+            while (true) {
+                switch (conn.getResponseCode()) {
+                    case HTTP_OK:
+                        return new BufferedInputStream(getInputStream(conn));
+                    case HTTP_MOVED_PERM:
+                    case HTTP_MOVED_TEMP:
+                    case HTTP_SEE_OTHER:
+                        if (redirectsCount++ > 50) {
+                            throw new IllegalStateException("too many redirects connection to [" + urlToGet + "]");
+                        }
+                        String location = conn.getHeaderField("Location");
+                        URL base = new URL(url);
+                        URL next = new URL(base, location);  // Deal with relative URLs
+                        url = next.toExternalForm();
+                        conn = createConnection(url);
+                        break;
+                    case HTTP_NOT_FOUND:
+                        throw new ResourceNotFoundException("{} not found", urlToGet);
+                    default:
+                        int responseCode = conn.getResponseCode();
+                        throw new ElasticsearchStatusException("error during downloading {}", RestStatus.fromCode(responseCode), urlToGet);
+                }
+            }
+        });
+    }
+
+
+     public static InputStream stringToInputStream(String input) {
+        // Convert the string to a byte array
+        byte[] bytes = input.getBytes();
+        // Create an InputStream from the byte array
+        InputStream inputStream = new ByteArrayInputStream(bytes);
+        return inputStream;
+    }
+    
+    InputStream post(String urlToGet, String method, String authorization, String content_type, 
+            String body, Map<String,String> parameters) throws IOException {
+        return doPrivileged(() -> {
+            String url = urlToGet;
+             // Write the JSON data to the output stream
+            byte[] postData = body.getBytes(StandardCharsets.UTF_8);
+            HttpURLConnection conn = createConnection(url, method, authorization, content_type, parameters);
+            conn.getOutputStream().write(postData);
+            int redirectsCount = 0;
+            while (true) {
+                switch (conn.getResponseCode()) {
+                    case HTTP_OK:
+                        return new BufferedInputStream(conn.getInputStream());
+                    case HTTP_MOVED_PERM:
+                    case HTTP_MOVED_TEMP:
+                    case HTTP_SEE_OTHER:
+                        if (redirectsCount++ > 50) {
+                            return new BufferedInputStream(stringToInputStream("too many redirects"));
+                            //throw new IllegalStateException("too many redirects connection to [" + urlToGet + "]");
+                        }
+/*                        String location = conn.getHeaderField("Location");
+                        URL base = new URL(url);
+                        URL next = new URL(base, location);  // Deal with relative URLs
+                        url = next.toExternalForm();
+                        conn = createConnection(url);
+*/
+                        break;
+                    case HTTP_NOT_FOUND:
+                        return new BufferedInputStream(stringToInputStream("url not found"));
+                        //throw new ResourceNotFoundException("{} not found", urlToGet);
+                    default:
+                        int responseCode = conn.getResponseCode();
+                        return new BufferedInputStream(stringToInputStream("error during downloading {} " + responseCode + " " + conn.getResponseMessage()));
+                        //throw new ElasticsearchStatusException("error during downloading {}", RestStatus.fromCode(responseCode), urlToGet);
+
+                }
+            }
+        });
+    }
+
+    @SuppressForbidden(reason = "we need socket connection to download data from internet")
+    private static InputStream getInputStream(HttpURLConnection conn) throws IOException {
+        return conn.getInputStream();
+    }
+
+    private static HttpURLConnection createConnection(String url) throws IOException {
+        HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+        conn.setConnectTimeout(10000);
+        conn.setReadTimeout(10000);
+        conn.setDoOutput(false);
+        conn.setInstanceFollowRedirects(false);
+        return conn;
+    }
+
+    private static HttpURLConnection createConnection(String url,String method, String authorization, 
+        String content_type, Map<String,String> parameters) throws IOException {
+        HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+        conn.setRequestMethod(method);
+        for (Map.Entry<String, String> entry : parameters.entrySet()) {
+                conn.setRequestProperty(entry.getKey(), entry.getValue());
+            }
+        conn.addRequestProperty("Authorization", authorization);
+        conn.addRequestProperty("Content-Type", content_type);
+        conn.setConnectTimeout(10000);
+        conn.setReadTimeout(10000);
+        conn.setDoOutput(true);
+        conn.setDoInput(true);
+        conn.setInstanceFollowRedirects(true);
+        return conn;
+    }
+
+    private static <R> R doPrivileged(CheckedSupplier<R, IOException> supplier) throws IOException {
+        SpecialPermission.check();
+        try {
+            return AccessController.doPrivileged((PrivilegedExceptionAction<R>) supplier::get);
+        } catch (PrivilegedActionException e) {
+            throw (IOException) e.getCause();
+        }
+    }
+}
--- src/main/java/org/elasticsearch/plugin/ingest/rest/IngestRestProcessor.java
+++ src/main/java/org/elasticsearch/plugin/ingest/rest/IngestRestProcessor.java
@@ -1,157 +1,141 @@
 package org.elasticsearch.plugin.ingest.rest;
 
+import org.elasticsearch.SpecialPermission;
 import org.elasticsearch.ingest.AbstractProcessor;
 import org.elasticsearch.ingest.IngestDocument;
 import org.elasticsearch.ingest.Processor;
 import org.elasticsearch.ingest.ConfigurationUtils;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicHeader;
-import org.apache.http.Header;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.nio.charset.StandardCharsets;
-import java.security.AccessController;
-import java.security.PrivilegedAction;
 import java.util.Map;
-import java.security.AccessController;
-import java.security.PrivilegedAction;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
+import java.io.InputStream;
 
 public class IngestRestProcessor extends AbstractProcessor {
-    public static final String TYPE = "ingest_rest";
+        public static final String TYPE = "ingest_rest";
+        public static final SpecialPermission INSTANCE = new SpecialPermission();
 
-    private final String field;
-    private final String targetField;
-    private final String model_id;
-    private final String task;
-    private final String authorization;
-    private final String content_type;
-    private final String endpoint;
-    private final String method;
+        private final String field;
+        private final String targetField;
+        private final String model_id;
+        private final String task;
+        private final String authorization;
+        private final String content_type;
+        private final String endpoint;
+        private final String method;
+        private final HttpClient httpClient;
 
-    public IngestRestProcessor(String tag, String description, String field,
-            String targetField, String model_id, String task, String authorization,
-            String content_type, String endpoint, String method) throws IOException {
-        super(tag, description);
-        this.field = field;
-        this.targetField = targetField;
-        this.model_id = model_id;
-        this.task = task;
-        this.authorization = authorization;
-        this.content_type = content_type;
-        this.endpoint = endpoint;
-        this.method = method;
-        // Set socket permissions programmatically
-//        setSocketPermissions();
-    }
-
-
-    String jsonString = "{\n" +
-    "  \"text\": \"Japanese pitcher and freshly minted Dodgers team member Shohei Ohtani’s decade-long $700 million contract with the team — already a subject of interest in the sports and tax worlds for its structure — has now drawn the ire of California’s controller.\",\n" +
-    "  \"question\": \"\",\n" +
-    "  \"context\": \"\",\n" +
-    "  \"instruction\": \"Extract entities from the model's output\"\n" +
-    "}";
-
-
-    private String MakeRestCall(String endpoint, String method, String body, String authorization, String content_type) {
-        StringBuilder response = new StringBuilder();
-        try {
-            // Create the URL
-            URL url = new URL(endpoint);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("POST");
-            conn.setRequestProperty("Content-Type", content_type);
-            conn.setRequestProperty("Authorization", authorization);
-            conn.setDoOutput(true);
-
-            // Write the JSON data to the output stream
-            byte[] postData = jsonString.getBytes(StandardCharsets.UTF_8);
-            conn.getOutputStream().write(postData);
-
-            // Read the response
-            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            response = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                response.append(line);
-            }
-            reader.close();
-
-            // Print the response
-            System.out.println("Response: " + response.toString());
-
-            // Close the connection
-            conn.disconnect();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return e.getMessage();
+        public IngestRestProcessor(String processorTag, String description, String field,
+                        String targetField, String model_id, String task, String authorization,
+                        String content_type, String endpoint, String method) throws IOException {
+                super(processorTag, description);
+                this.field = field;
+                this.targetField = targetField;
+                this.model_id = model_id;
+                this.task = task;
+                this.authorization = authorization;
+                this.content_type = content_type;
+                this.endpoint = endpoint;
+                this.method = method;
+                this.httpClient = new HttpClient();
         }
-        return response.toString();
-    }
 
+        String jsonString = "{\n" +
+                        "  \"text\": \"Japanese pitcher and freshly minted Dodgers team member Shohei Ohtani’s decade-long $700 million contract with the team — already a subject of interest in the sports and tax worlds for its structure — has now drawn the ire of California’s controller.\",\n"
+                        +
+                        "  \"question\": \"\",\n" +
+                        "  \"context\": \"\",\n" +
+                        "  \"instruction\": \"Extract entities from the model's output\"\n" +
+                        "}";
 
-    @Override
-    public IngestDocument execute(IngestDocument ingestDocument) throws Exception {
-        IngestDocument document = ingestDocument;
-        if (document.hasField(field)) {
-            String value = document.getFieldValue(field, String.class);
-            String response = MakeRestCall(this.endpoint, this.method, value, this.authorization, this.content_type);
-            document.setFieldValue(targetField, response);
-            // implement the processor logic here
+        private String MakeRestCall(String endpoint, String method, String body, String authorization,
+                        String content_type, Map<String,String> parameters) {
+                StringBuilder response = new StringBuilder();
+                try {
+                        InputStream is = httpClient.post(endpoint, method, authorization, content_type, body, parameters);
+                        // Read the response
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+                        response = new StringBuilder();
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                                response.append(line);
+                        }
+                        reader.close();
+
+                        // Print the response
+                        System.out.println("Response: " + response.toString());
+
+                } catch (Exception e) {
+                        e.printStackTrace();
+                        return e.getMessage();
+                }
+                return response.toString();
         }
-        return document;
-    }
 
-    @Override
-    public String getType() {
-        return TYPE;
-    }
-
-    public static final class Factory implements Processor.Factory {
         @Override
-        public IngestRestProcessor create(Map<String, Processor.Factory> registry,
-                String processorTag, String description, Map<String, Object> config) throws IOException {
-
-            String field = ConfigurationUtils.readStringProperty(TYPE, processorTag, config, "field",
-                    "default_field_name");
-            String targetField = ConfigurationUtils.readStringProperty(TYPE, processorTag, config, "target_field",
-                    "default_target_field_name");
-            String model_id = ConfigurationUtils.readStringProperty(TYPE, processorTag, config, "model_id",
-                    "7e009044-17cd-4132-b43f-cfd5cc5c61fd");
-            String task = ConfigurationUtils.readStringProperty(TYPE, processorTag, config, "task",
-                    "named_entity_recognition");
-            String authorization = ConfigurationUtils.readStringProperty(TYPE, processorTag, config, "authorization",
-                    "Basic bWljaGFlbC5jaXptYXJAbWNwbHVzYS5jb206bWljaGFlbA==");
-            String content_type = ConfigurationUtils.readStringProperty(TYPE, processorTag, config, "content_type",
-                    "application/json");
-            String endpoint = ConfigurationUtils.readStringProperty(TYPE, processorTag, config, "endpoint",
-                    "https://control-plane-gateway-44gp1iu3.uc.gateway.dev/mcplusa/models/predict");
-            String method = ConfigurationUtils.readStringProperty(TYPE, processorTag, config, "method",
-                    "POST");
-            
-            return new IngestRestProcessor(processorTag, description, field, targetField, model_id, task, authorization,
-                    content_type, endpoint, method);
+        public IngestDocument execute(IngestDocument ingestDocument) throws Exception {
+                IngestDocument document = ingestDocument;
+                if (document.hasField(field)) {
+                        String value = document.getFieldValue(field, String.class);
+                        Map<String,String> parameters = Map.of(
+                                "model_id", this.model_id,
+                                "task", this.task
+                        );
+                        String response = MakeRestCall(this.endpoint, this.method, jsonString, this.authorization,
+                                        this.content_type, parameters);
+                        document.setFieldValue(targetField, response);
+                        // implement the processor logic here
+                }
+                return document;
         }
-    }
 
-    /*
-     * Headers should be in format of JSON string. For example:
-     * headers: '{"Authorization":
-     * "Basic bWljaGFlbC5jaXptYXJAbWNwbHVzYS5jb206bWljaGFlbA==",
-     * "Content-Type": "application/json"}'
-     */
+        @Override
+        public String getType() {
+                return TYPE;
+        }
+
+        public static final class Factory implements Processor.Factory {
+                @Override
+                public IngestRestProcessor create(Map<String, Processor.Factory> registry,
+                                String processorTag, String description, Map<String, Object> config)
+                                throws IOException {
+
+                        String field = ConfigurationUtils.readStringProperty(TYPE, processorTag, config, 
+                                        "field",
+                                        "foo");
+                        String targetField = ConfigurationUtils.readStringProperty(TYPE, processorTag, config,
+                                        "target_field",
+                                        "bar");
+                        String model_id = ConfigurationUtils.readStringProperty(TYPE, processorTag, config, 
+                                        "model_id",
+                                        "7e009044-17cd-4132-b43f-cfd5cc5c61fd");
+                        String task = ConfigurationUtils.readStringProperty(TYPE, processorTag, config, 
+                                        "task",
+                                        "named_entity_recognition");
+                        String authorization = ConfigurationUtils.readStringProperty(TYPE, processorTag, config,
+                                        "authorization",
+                                        "Basic bWljaGFlbC5jaXptYXJAbWNwbHVzYS5jb206bWljaGFlbA==");
+                        String content_type = ConfigurationUtils.readStringProperty(TYPE, processorTag, config,
+                                        "content_type",
+                                        "application/json");
+                        String endpoint = ConfigurationUtils.readStringProperty(TYPE, processorTag, config, 
+                                        "endpoint",
+                                        "https://control-plane-gateway-44gp1iu3.uc.gateway.dev/mcplusa/models/predict");
+                        String method = ConfigurationUtils.readStringProperty(TYPE, processorTag, config, 
+                                        "method",
+                                        "POST");
+                        return new IngestRestProcessor(processorTag, description, field, targetField, model_id, task,
+                                        authorization,
+                                        content_type, endpoint, method);
+                }
+        }
+
+        /*
+         * Headers should be in format of JSON string. For example:
+         * headers: '{"Authorization":
+         * "Basic bWljaGFlbC5jaXptYXJAbWNwbHVzYS5jb206bWljaGFlbA==",
+         * "Content-Type": "application/json"}'
+         */
 
 }
